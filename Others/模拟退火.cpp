ac2f11043857c7488692995c4f868d87217bfa3b
bandiaoz <893396998@qq.com>
2022-04-28 21:06:47 +0800

    update

--- Others/模拟退火.cpp
+++ Others/模拟退火.cpp
@@ -1,40 +1,56 @@
-#include <cmath>
-#include <iostream>
+#include <bits/stdc++.h>
+
 using namespace std;
-#define eps 1e-8
-#define IO ios::sync_with_stdio(false), cin.tie(0)
-//Gym - 101981D
-const int maxn = 105;
-int n;
-struct point {
-    double x, y, z;
-} p[maxn];
+using ll = long long;
 
-double dis(point a, point b) {
-    return sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y) + (a.z - b.z) * (a.z - b.z));
-}
-
-double solve() {
-    double step = 10000, ans = 1e30;
-    point xx;
-    xx.x = xx.y = xx.z = 0;
-    int pos = 1;
-    while (step > eps) {
-        for (int i = 1; i <= n; ++i)  //找一个最远的点
-            if (dis(xx, p[pos]) < dis(xx, p[i])) pos = i;
-        double mt = dis(xx, p[pos]);
-        ans = min(ans, mt);
-        xx.x += (p[pos].x - xx.x) / mt * step;
-        xx.y += (p[pos].y - xx.y) / mt * step;
-        xx.z += (p[pos].z - xx.z) / mt * step;
-        step *= 0.98;
-    }
-    return ans;
-}
+const double eps = 1e-8;
 
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int n;
     cin >> n;
-    for (int i = 1; i <= n; ++i)
-        cin >> p[i].x >> p[i].y >> p[i].z;
-    printf("%.8lf\n", solve());
+
+    vector<tuple<int, int, int>> a(n);
+    for (int i = 0; i < n; ++i) {
+        int x, y, z;
+        cin >> x >> y >> z;
+        a[i] = tuple(x, y, z);
+    }
+
+    auto solve = [&]() {
+        double step = 10000, ans = 1e30;
+        tuple<double, double, double> tp;
+        int pos = 0;
+
+        auto dis = [&](auto A, auto B) {
+            auto [x1, y1, z1] = A;
+            auto [x2, y2, z2] = B;
+            return sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) + (z2 - z1) * (z2 - z1));
+        };
+
+        while (step > eps) {
+            for (int i = 0; i < n; ++i) { //找一个最远的点
+                if (dis(tp, a[pos]) < dis(tp, a[i])) {
+                    pos = i;
+                }
+            }  
+            double mt = dis(tp, a[pos]);
+            ans = min(ans, mt);
+            auto [x, y, z] = tp;
+            auto [px, py, pz] = a[pos];
+            x += (px - x) / mt * step;
+            y += (py - y) / mt * step;
+            z += (pz - z) / mt * step;
+            tp = tuple(x, y, z);
+
+            step *= 0.98;
+        }
+        return ans;
+    };
+
+    cout << fixed << setprecision(8) << solve() << "\n";
+
+    return 0;
 }
