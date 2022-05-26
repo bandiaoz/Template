625a328efa0cec80a8114f00f084ce7ba63bba7c
bandiaoz <893396998@qq.com>
2022-05-26 16:36:12 +0800

    update

--- Graph/2sat.cpp
+++ Graph/2sat.cpp
@@ -5,12 +5,12 @@ using ll = long long;
 
 struct TwoSat {
     int n;
-    vector<vector<int>> e;
+    vector<vector<int>> G;
     vector<bool> ans;
-    TwoSat(int n) : n(n), e(2 * n), ans(n) {}
+    TwoSat(int n) : n(n), G(2 * n), ans(n) {}
     void addClause(int u, bool f, int v, bool g) {
-        e[2 * u + !f].push_back(2 * v + g);
-        e[2 * v + !g].push_back(2 * u + f);
+        G[2 * u + !f].push_back(2 * v + g);
+        G[2 * v + !g].push_back(2 * u + f);
     }
     bool satisfiable() {
         vector<int> id(2 * n, -1), dfn(2 * n, -1), low(2 * n, -1);
@@ -19,7 +19,7 @@ struct TwoSat {
         function<void(int)> tarjan = [&](int u) {
             stk.push_back(u);
             dfn[u] = low[u] = now++;
-            for (auto v : e[u]) {
+            for (auto v : G[u]) {
                 if (dfn[v] == -1) {
                     tarjan(v);
                     low[u] = min(low[u], low[v]);
