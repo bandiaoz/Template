8483f151133a2b9711bd9ee1d7ff626c9b4f08bd
bandiaoz <893396998@qq.com>
2022-06-28 22:39:05 +0800

    update

--- String/kmp.cpp
+++ String/kmp.cpp
@@ -1,160 +1,53 @@
-#pragma region
-#include <algorithm>
-#include <cmath>
-#include <cstring>
-#include <iomanip>
-#include <iostream>
-#include <map>
-#include <queue>
-#include <set>
-#include <stack>
-#include <string>
-#include <vector>
+#include <bits/stdc++.h>
+
 using namespace std;
-typedef long long ll;
-#define rep(i, a, n) for (int i = a; i <= n; ++i)
-#define per(i, a, n) for (int i = n; i >= a; --i)
-namespace fastIO {
-#define BUF_SIZE 100000
-#define OUT_SIZE 100000
-//fread->R
-bool IOerror = 0;
-//inline char nc(){char ch=getchar();if(ch==-1)IOerror=1;return ch;}
-inline char nc() {
-    static char buf[BUF_SIZE], *p1 = buf + BUF_SIZE, *pend = buf + BUF_SIZE;
-    if (p1 == pend) {
-        p1 = buf;
-        pend = buf + fread(buf, 1, BUF_SIZE, stdin);
-        if (pend == p1) {
-            IOerror = 1;
-            return -1;
-        }
+using ll = long long;
+
+vector<int> prefixFunction(string s) {
+    int n = (int)s.size();
+    vector<int> p(n);
+    for (int i = 1; i < n; ++i) {
+        int j = p[i - 1];
+        while (j > 0 && s[i] != s[j]) j = p[j - 1];
+        if (s[i] == s[j]) ++j;
+        p[i] = j;
     }
-    return *p1++;
+    return p;
 }
-inline bool blank(char ch) { return ch == ' ' || ch == '\n' || ch == '\r' || ch == '\t'; }
-template <class T>
-inline bool R(T &x) {
-    bool sign = 0;
-    char ch = nc();
-    x = 0;
-    for (; blank(ch); ch = nc())
-        ;
-    if (IOerror)
-        return false;
-    if (ch == '-')
-        sign = 1, ch = nc();
-    for (; ch >= '0' && ch <= '9'; ch = nc())
-        x = x * 10 + ch - '0';
-    if (sign)
-        x = -x;
-    return true;
-}
-inline bool R(double &x) {
-    bool sign = 0;
-    char ch = nc();
-    x = 0;
-    for (; blank(ch); ch = nc())
-        ;
-    if (IOerror)
-        return false;
-    if (ch == '-')
-        sign = 1, ch = nc();
-    for (; ch >= '0' && ch <= '9'; ch = nc())
-        x = x * 10 + ch - '0';
-    if (ch == '.') {
-        double tmp = 1;
-        ch = nc();
-        for (; ch >= '0' && ch <= '9'; ch = nc())
-            tmp /= 10.0, x += tmp * (ch - '0');
+
+// KMP based on prefixFunction. return all match postion in t
+// also can create string st = s + '#' + t, and call prefixFunction(st),
+// if p[i] == s.length()，it's a successful match: s in t
+vector<int> kmp(string s, string t) {
+    vector<int> ans;
+    int n = (int)s.size(), m = (int)t.size();
+    if (n > m) return ans;
+    auto p = prefixFunction(s);
+    for (int i = 0, j = 0; i < m; ++i) {
+        while (j > 0 && s[j] != t[i]) j = p[j - 1];
+        if (s[j] == t[i] && ++j == n) ans.emplace_back(i - n + 1);
     }
-    if (sign)
-        x = -x;
-    return true;
+    return ans;
 }
-inline bool R(char *s) {
-    char ch = nc();
-    for (; blank(ch); ch = nc())
-        ;
-    if (IOerror)
-        return false;
-    for (; !blank(ch) && !IOerror; ch = nc())
-        *s++ = ch;
-    *s = 0;
-    return true;
-}
-inline bool R(char &c) {
-    c = nc();
-    if (IOerror) {
-        c = -1;
-        return false;
-    }
-    return true;
-}
-template <class T, class... U>
-bool R(T &h, U &... t) { return R(h) && R(t...); }
-#undef OUT_SIZE
-#undef BUF_SIZE
-};  // namespace fastIO
-using namespace fastIO;
-template <class T>
-void _W(const T &x) { cout << x; }
-void _W(const int &x) { printf("%d", x); }
-void _W(const int64_t &x) { printf("%lld", x); }
-void _W(const double &x) { printf("%.16f", x); }
-void _W(const char &x) { putchar(x); }
-void _W(const char *x) { printf("%s", x); }
-template <class T, class U>
-void _W(const pair<T, U> &x) { _W(x.F), putchar(' '), _W(x.S); }
-template <class T>
-void _W(const vector<T> &x) {
-    for (auto i = x.begin(); i != x.end(); _W(*i++))
-        if (i != x.cbegin()) putchar(' ');
-}
-void W() {}
-template <class T, class... U>
-void W(const T &head, const U &... tail) { _W(head), putchar(sizeof...(tail) ? ' ' : '\n'), W(tail...); }
-#pragma endregion
-const int N = 1e5 + 10;
-int nextt[N];  //nextt[i]表示的是前i的字符组成的这个子串最长的相同前缀后缀的长度！
-// aaabbababaabab
-// aabbba
-void getNext(string str) {
-    int len = str.size(), k = -1, j = 0;
-    nextt[0] = -1;
-    while (j < len) {
-        if (k == -1 || str[j] == str[k]) {
-            j++, k++;
-            nextt[j] = k;
-        } else
-            k = nextt[k];
-    }
-}
-int match(string S, string P) {
-    int lenS = S.size();
-    int lenP = P.size();
-    int j = 0, k = 0;
-    while (j < lenS && k < lenP) {
-        if (k == -1 || S[j] == P[k])
-            j++, k++;
-        else
-            k = nextt[k];
-    }
-    if (k < lenP)
-        return -1;
-    else
-        return j - lenP;
-}
+
 int main() {
-    int T;
-    cin >> T;
-    while (T--) {
-        string s, t;
-        cin >> s >> t;
-        getNext(t);
-        cout << match(s, t) << endl;
-        for (int i = 0; i <= t.length(); ++i)
-            cout << nextt[i] << " ";
-        cout << endl;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string s, t;
+    cin >> s >> t;
+
+    auto ans = kmp(t, s);
+    for (int i = 0; i < ans.size(); ++i) {
+        cout << ans[i] + 1 << "\n";
     }
+
+    auto p = prefixFunction(t);
+    for (int i = 0; i < p.size(); ++i) {
+        cout << p[i] << " \n"[i == p.size() - 1];
+    }
+
+    return 0;
 }
+
+// test problem: https://www.luogu.com.cn/problem/P3375
