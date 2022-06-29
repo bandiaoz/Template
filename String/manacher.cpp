36bbc54d2f518c85d26f387805e5f20e2d4a63c8
bandiaoz <893396998@qq.com>
2022-06-29 18:39:01 +0800

    update

--- String/manacher.cpp
+++ String/manacher.cpp
@@ -1,39 +1,68 @@
 #include <bits/stdc++.h>
-using namespace std;
-const int N = 200005 << 1;
-char T[N];
-char S[N];
-int R[N];
 
-void Init(char *T)
-{
-    S[0] = '$';
-    int len = strlen(T);
-    for (int i = 0; i <= len; i++)
-    {
-        S[2 * i + 1] = '#';
-        S[2 * i + 2] = T[i];
+using namespace std;
+using ll = long long;
+
+template <typename T>
+vector<int> manacher(int n, const T &s) {
+    if (n == 0) {
+        return vector<int>();
     }
+    vector<int> res(2 * n - 1, 0);
+    int l = -1, r = -1;
+    for (int z = 0; z < 2 * n - 1; z++) {
+        int i = (z + 1) >> 1;
+        int j = z >> 1;
+        int p = (i >= r ? 0 : min(r - i, res[2 * (l + r) - z]));
+        while (j + p + 1 < n && i - p - 1 >= 0) {
+            if (!(s[j + p + 1] == s[i - p - 1])) {
+                break;
+            }
+            p++;
+        }
+        if (j + p > r) {
+            l = i - p;
+            r = j + p;
+        }
+        res[z] = p;
+    }
+    return res;
+    // res[2 * i] = odd radius in position i
+    // res[2 * i + 1] = even radius between positions i and i + 1
+    // s = "abaa" -> res = {0, 0, 1, 0, 0, 1, 0}
+    // s = "aaa" -> res = {0, 1, 1, 1, 0}
+    // in other words, for every z from 0 to 2 * n - 2:
+    // calculate i = (z + 1) >> 1 and j = z >> 1
+    // now there is a palindrome from i - res[z] to j + res[z]
+    // (watch out for i > j and res[z] = 0)
+}
+template <typename T>
+vector<int> manacher(const T &s) {
+    return manacher((int)s.size(), s);
 }
 
-void Manacher(char *S)
-{
-    int k = 0, mx = 0;
-    int len = strlen(S);
-    for (int i = 0; i < len; i++)
-    {
-        if (mx > i)
-            R[i] = R[2 * k - i] < mx - i ? R[2 * k - i] : mx - i;
-        else
-            R[i] = 1;
-        while (S[i + R[i]] == S[i - R[i]])
-            R[i]++;
-        if (R[i] + i > mx)
-        {
-            mx = R[i] + i;
-            k = i;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string s;
+    cin >> s;
+    int n = s.length();
+
+    auto ans = manacher(s);
+
+    int len = 0, id = -1;
+    for (int z = 0; z < 2 * n - 1; ++z) {
+        if (z % 2 == 0 && 1 + 2 * ans[z] > len) { // odd length of palindrome
+            len = 1 + 2 * ans[z];
+            id = z / 2 - ans[z];
+        } else if (z % 2 == 1 && 2 * ans[z] > len) { // even length of palindrome
+            len = 2 * ans[z];
+            id = z / 2 - ans[z] + 1;
         }
     }
-}
 
-int main() {}
+    cout << s.substr(id, len) << "\n";
+
+    return 0;
+}
