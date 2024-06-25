d6e1dbd55a6f4cf32e5da79a8537d74e1216c88a
bandiaoz <893396998@qq.com>
2024-06-26 01:08:00 +0800

    docs: kmp

--- String/kmp.cpp
+++ String/kmp.cpp
@@ -18,9 +18,9 @@ std::vector<int> prefixFunction(std::string s) {
 
 /**
  * @brief KMP based on prefixFunction. 查找模式串 s 在文本串 t 中所有出现的位置
- * @param s string 模式串
- * @param t string 文本串
- * @return return all match postion in t
+ * @param s string 模式串，长度为 n
+ * @param t string 文本串，长度为 m
+ * @return ans[i] 表示 s[0:n] 在 t[ans[i]:ans[i]+n] 中出现
  * @note also can create string st = s + '#' + t, and call prefixFunction(st), if p[i] == s.length()，it's a successful match: s in t
  * @link https://www.luogu.com.cn/problem/P3375
 */
@@ -34,40 +34,4 @@ std::vector<int> kmp(std::string s, std::string t) {
         if (s[j] == t[i] && ++j == n) ans.emplace_back(i - n + 1);
     }
     return ans;
-}
-
-int main() {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-
-    std::string t, s;
-    std::cin >> t >> s;
-
-    if (true) { 
-        // method 1: st = s + '#' + t, and call prefixFunction(st)
-        std::string st = s + '#' + t;
-        auto p = prefixFunction(st);
-        for (int i = s.length() + 1; i < st.length(); i++) {
-            if (p[i] == s.length()) {
-                std::cout << i - 2 * s.length() + 1 << "\n";
-            }
-        }
-
-        for (int i = 0; i < s.length(); i++) {
-            std::cout << p[i] << " \n"[i == s.length() - 1];
-        }
-    } else { 
-        // method 2: call kmp(s, t)
-        auto ans = kmp(s, t);
-        for (int i = 0; i < ans.size(); i++) {
-            std::cout << ans[i] + 1 << "\n";
-        }
-
-        auto p = prefixFunction(s);
-        for (int i = 0; i < p.size(); i++) {
-            std::cout << p[i] << " \n"[i == p.size() - 1];
-        }
-    }
-
-    return 0;
 }
