adc57c00672da4f9ba785f74650ab04172596c2b
bandiaoz <893396998@qq.com>
2024-06-29 19:36:45 +0800

    docs: dijkstra

--- Graph/dijkstra.cpp
+++ Graph/dijkstra.cpp
@@ -1,11 +1,8 @@
 #include <bits/stdc++.h>
 
 /**
- * @brief dijkstra algorithm
+ * @brief 单源最短路 dijkstra algorithm
  * @tparam T type of distance
- * @param g 
- * @param w 
- * @param start 
  * @return distance from start to each vertex
  * @link dist: https://www.luogu.com.cn/problem/P4779
  * @link path: https://judge.yosupo.jp/problem/shortest_path
@@ -13,7 +10,7 @@
  */
 template <typename T>
 auto dijkstra(const std::vector<std::vector<std::pair<int, int>>> &g,
-              const std::vector<T> &w, int start) -> std::pair<std::vector<int64_t>, std::vector<int>> {
+              const std::vector<T> &w, int start) {
     std::vector<int64_t> dis(g.size(), -1);
     std::vector<int> pre(g.size(), -1);
     constexpr int mod = 100003;
@@ -37,5 +34,5 @@ auto dijkstra(const std::vector<std::vector<std::pair<int, int>>> &g,
             q.emplace(d - w[j], v, u);
         }
     }
-    return {dis, pre};
+    return std::pair{dis, pre};
 }
