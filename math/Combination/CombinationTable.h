c5a074f6ab7571c01d6eca95db207021d048835a
bandiaoz <893396998@qq.com>
2024-08-19 13:40:17 +0800

    update

--- math/Combination/CombinationTable.h
+++ math/Combination/CombinationTable.h
@@ -29,36 +29,60 @@ namespace OY {
             m_factorial_inv[range] = m_factorial[range].inv();
             for (uint32_t i = range - 1; ~i; i--) m_factorial_inv[i] = m_factorial_inv[i + 1] * Tp::raw(i + 1);
         }
-        Tp comb(uint32_t n, uint32_t m) const {
+        /**
+         * @brief 计算组合数
+         */
+        Tp binom(uint32_t n, uint32_t m) const {
             if (n < m) return Tp::raw(0);
             if (n == m) return Tp::raw(1);
             return m_factorial[n] * m_factorial_inv[m] * m_factorial_inv[n - m];
         }
+        /**
+         * @brief 计算全排列数，即 n!
+         */
         Tp perm(uint32_t n) const { return m_factorial[n]; }
+        /**
+         * @brief 计算排列数
+         */
         Tp perm(uint32_t n, uint32_t m) const {
             if (n < m) return Tp::raw(0);
             if (n == m) return m_factorial[n];
             return m_factorial[n] * m_factorial_inv[n - m];
         }
+        /**
+         * @brief 计算多重集的全排列数
+         */
         template <typename Iterator>
         Tp perm_multi(Iterator first, Iterator last) const {
             Tp res(m_factorial[std::accumulate(first, last, 0)]);
             for (auto it = first; it != last; ++it) res *= m_factorial_inv[*it];
             return res;
         }
+        /**
+         * @brief 计算不相邻的组合数，即 binom(n - m + 1, m)
+         */
         Tp comb_nonadjacent(uint32_t n, uint32_t m) const {
             if (n < m) return Tp::raw(0);
-            return comb(n - m + 1, m);
+            return binom(n - m + 1, m);
         }
+        /**
+         * @brief 计算全错排列数
+         */
         Tp perm_staggered(uint32_t n) const {
             static std::vector<Tp> s_res{1, 0};
             while (s_res.size() < n + 1) s_res.push_back((s_res[s_res.size() - 2] + s_res.back()) * Tp::raw(s_res.size() - 1));
             return s_res[n];
         }
+        /**
+         * @brief 计算圆上全排列，即 (n - 1)!
+         */
         Tp perm_circular(uint32_t n) const { return m_factorial[n - 1]; }
+        /**
+         * @brief 计算圆上排列数，即 binom(n, m) * (m - 1)!
+         */
         Tp perm_circular(uint32_t n, uint32_t m) const {
             if (!m) return Tp::raw(1);
-            return comb(n, m) * m_factorial[m - 1];
+            return binom(n, m) * m_factorial[m - 1];
         }
     };
 }
