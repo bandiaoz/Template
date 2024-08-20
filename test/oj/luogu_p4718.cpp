c2c1f030d3090e41d2c36e8a01b8e6e61d1b4994
bandiaoz <893396998@qq.com>
2024-08-20 21:34:53 +0800

    update

---
+++ test/oj/luogu_p4718.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include "Math/NumberTheory/PollardRho.h"
+
+/**
+ * [P4718【模板】Pollard-Rho](https://www.luogu.com.cn/problem/P4718)
+ */
+
+void solve() {
+    uint64_t n;
+    std::cin >> n;
+
+    uint64_t max = 0;
+    OY::PollardRho::enumerate_prime_factors(n, [&](uint64_t prime) {
+        max = std::max(max, prime);
+    });
+
+    if (max == n) {
+        std::cout << "Prime\n";
+    } else {
+        std::cout << max << '\n';
+    }
+}
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
