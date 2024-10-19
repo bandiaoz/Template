1b07236ea84273ae921072388cb8ceeb390feb29
bandiaoz <893396998@qq.com>
2024-10-19 23:45:53 +0800

    update

---
+++ test/oj/Math/poly/luogu_p4781.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include "src/Math/ModInt/StaticModInt32.h"
+#include "src/Math/Poly/LagrangeInterpolation.h"
+
+using Z = OY::mint998244353;
+
+/*
+[P4781 【模板】拉格朗日插值](https://www.luogu.com.cn/problem/P4781)
+[status](https://www.luogu.com.cn/record/183385361)
+*/
+/**
+ * 本题为拉格朗日插值的模板题
+ */
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int n, k;
+    std::cin >> n >> k;
+
+    OY::LagrangeInterpolation<Z> li(n);
+    for (int i = 0; i < n; i++) {
+        int x, y;
+        std::cin >> x >> y;
+        li.addPoint(x, y);
+    }
+
+    li.prepare();
+    std::cout << li.calc(k) << '\n';
+    
+    return 0;
+}
